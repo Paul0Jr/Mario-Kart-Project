01f4bdbb93cd6cb23f0cfcfa7b01a4f99e48c793
Paulo <pjunior954@gmail.com>
2026-02-20 15:00:37 -0300

    Models

---
+++ Models/Player.js
@@ -0,0 +1,12 @@
+export default class Player {
+    constructor(config){
+        this.NAME = config.name;
+        this.SPEED = config.speed;
+        this.HANDLING = config.handling;
+        this.POWER = config.power;
+        this.POINTS = config.points
+    }
+
+    
+    
+}
