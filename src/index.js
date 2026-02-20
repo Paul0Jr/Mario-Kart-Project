277dc7e16b4cf8f656237d69a08e61dcf3dbf87e
Paulo <pjunior954@gmail.com>
2026-02-20 15:01:37 -0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,39 +1,17 @@
-import Player from './Player.js';
+import Player from '../Models/Player.js';
+import { playRaceEngine } from '../Services/RaceEngine.js';
 
-const players= [
-    new Player({ name: "Mario", speed: 4, handling: 3, power: 3, points: 0}),
-    new Player({ name: "Luigi", speed: 3, handling: 4, power: 4, points: 0}),
-    new Player({ name: "Peach", speed: 3, handling: 4, power: 2, points: 0}),
-    new Player({ name: "Bowser", speed: 5, handling: 2, power: 5, points: 0}),
-    new Player({ name: "Yoshi", speed: 2, handling: 4, power: 3, points: 0}),
-    new Player({ name: "Donkey Kong", speed: 2, handling: 2, power: 5, points: 0})
-
-]
-
-async function rollDice(){
-    return Math.floor(Math.random() * 6) + 1;
-}
-
-
-async function getRandomPlayers() {
-    const player1 = players[Math.floor(Math.random() * players.length)];
-    let player2;
-    
-    do {
-        player2 = players[Math.floor(Math.random() * players.length)];
-    } while (player2 === player1);
-    
-    return { player1, player2 };
-}
-
-async function playRaceEngine(char1, char2) {
-
-}
+const players = [
+    new Player({ name: "Mario", speed: 4, handling: 3, power: 3, points: 0 }),
+    new Player({ name: "Luigi", speed: 3, handling: 4, power: 4, points: 0 }),
+    new Player({ name: "Peach", speed: 3, handling: 4, power: 2, points: 0 }),
+    new Player({ name: "Bowser", speed: 5, handling: 2, power: 5, points: 0 }),
+    new Player({ name: "Yoshi", speed: 2, handling: 4, power: 3, points: 0 }),
+    new Player({ name: "Donkey Kong", speed: 2, handling: 2, power: 5, points: 0 })
+];
 
 (async function main() {
-    const { player1, player2 } = await getRandomPlayers();
     console.log("---------- Bem-vindo ao Mario-Kart!! -----------");
-    console.log(`Começando nova corrida entre ${player1.NAME} X ${player2.NAME}`);
-    await playRaceEngine(player1, player2);
-    
+    console.log(`${players.length} jogadores, ${players.length - 1} rodadas, ${players.length / 2} confrontos por rodada`);
+    await playRaceEngine(players);
 })()
